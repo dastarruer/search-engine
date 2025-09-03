9662e2c3eb2a05fab969bb61fe97b43fac91e1e2
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-09-03 14:24:40 -0400

    Create function to make get request

--- crawler/src/main.rs
+++ crawler/src/main.rs
@@ -1,13 +1,31 @@
-use std::collections::HashMap;
+use reqwest::IntoUrl;
 use tokio;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://www.rust-lang.org")
-        .await?
-        .text()
-        .await?;
+    let html = make_get_request("https://www.rust-lang.org").await.unwrap();
 
-    println!("{resp:#?}");
+    println!("{html:#?}");
     Ok(())
+}
+
+/// Make a get request to a specific URL.
+/// This (should) return the HTML of the URL.
+async fn make_get_request(url: impl IntoUrl) -> Result<String, Box<dyn std::error::Error>> {
+    Ok(reqwest::get(url).await?.text().await?)
+}
+
+#[cfg(test)]
+mod test {
+    mod make_get_request {
+        use super::super::make_get_request;
+
+        // Instead of using #[test], we use #[tokio::test] so we can test async functions
+        #[tokio::test]
+        async fn test_basic_site() {
+            let html = make_get_request("https://crawler-test.com/status_codes/status_200").await.unwrap();
+
+            assert!(html.contains("Status code 200 body"))
+        }
+    }
 }
