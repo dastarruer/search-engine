e76902ee7f62807f785a8a2632865930b1ba70ec
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-09-03 16:40:40 -0400

    WIP: Add function to extract URLs
    For now, just figured out how to read a file...

--- crawler/src/main.rs
+++ crawler/src/main.rs
@@ -15,6 +15,10 @@ async fn make_get_request(url: impl IntoUrl) -> Result<String, Box<dyn std::erro
     Ok(reqwest::get(url).await?.text().await?)
 }
 
+fn extract_urls_from_html(html: String) -> Vec<String> {
+    todo!()
+}
+
 #[cfg(test)]
 mod test {
     mod make_get_request {
@@ -23,9 +27,38 @@ mod test {
         // Instead of using #[test], we use #[tokio::test] so we can test async functions
         #[tokio::test]
         async fn test_basic_site() {
-            let html = make_get_request("https://crawler-test.com/status_codes/status_200").await.unwrap();
+            let html = make_get_request("https://crawler-test.com/status_codes/status_200")
+                .await
+                .unwrap();
 
             assert!(html.contains("Status code 200 body"))
+        }
+    }
+
+    mod extract_urls_from_html {
+        use std::{fs::File, io::Read, path::PathBuf};
+
+        use super::super::extract_urls_from_html;
+
+        #[test]
+        fn extract_single_href() {
+            let html_file_name = "extract_single_href.html";
+
+            // CARGO_MANIFEST_DIR gets the source dir of the project
+            let html_file = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+                .join("src")
+                .join("test-files")
+                .join(html_file_name);
+
+            let mut html = File::open(html_file)
+                .expect(format!("'{}' should exist.", html_file_name).as_str());
+
+            let mut buf = String::new();
+            html.read_to_string(&mut buf).unwrap();
+
+            let urls = extract_urls_from_html(buf);
+
+            assert_eq!(urls, vec!["https://www.wikipedia.org/"])
         }
     }
 }
