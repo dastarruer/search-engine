34cba52a0619f78067fd3553c3a8a2a12e780246
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-09-08 10:57:55 -0400

    Rename benchmark

--- crawler/benches/crawler.rs
+++ crawler/benches/crawler.rs
@@ -4,7 +4,7 @@ use url::Url;
 
 fn bench_crawl_from_url(c: &mut Criterion) {
     let url = Url::parse("https://books.toscrape.com/").unwrap();
-    c.bench_function("fib 20", |b| {
+    c.bench_function("crawl_from_url", |b| {
         b.iter(async || {
             let mut crawler = Crawler::new(url.clone());
             crawler.crawl_from_url(url.clone()).await.unwrap();
