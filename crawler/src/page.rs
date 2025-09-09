78193719e4f85fffecf74497ce750a393080b3cc
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-09-08 22:23:55 -0400

    Add constructor for CrawledPage

--- crawler/src/page.rs
+++ crawler/src/page.rs
@@ -1,16 +1,26 @@
 use url::Url;
 
 #[derive(PartialEq)]
+#[allow(unused)]
 pub struct Page {
-    pub url: Url,
+    url: Url,
 }
 
+#[allow(unused)]
 pub struct CrawledPage {
     url: Url,
 }
 
+#[allow(unused)]
 impl Page {
     pub fn new(url: Url) -> Self {
         Page { url }
+    }
+}
+
+#[allow(unused)]
+impl CrawledPage {
+    pub fn new(page: Page) -> Self {
+        CrawledPage { url: page.url }
     }
 }
