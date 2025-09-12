c6f064e0d60b044e19b9b48951bab3763bbf56bd
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-09-12 10:03:05 -0400

    Create pages table in db

---
+++ crawler/migrations/V1__Create_page_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE pages (
+    id serial NOT NULL, -- serial is autoincrementing
+    url text NOT NULL UNIQUE,
+    html text NOT NULL,
+    is_indexed bool NOT NULL,
+    http_status smallint NOT NULL, -- Store the http error code
+    PRIMARY KEY (id) -- Makes the id the identifier for each row
+);
