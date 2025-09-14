eed66a6899d249f0fc6fc490c950e89e885cd191
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-09-13 22:56:47 -0400

    Move httpserver to utils.rs
    Lets me use this across modules

---
+++ crawler/src/utils.rs
@@ -0,0 +1,32 @@
+// TODO: Figure out how to mark this as tests/benchmarks only, so httpmock becomes a dev-dependency
+use std::path::PathBuf;
+
+use httpmock::prelude::*;
+use url::Url;
+
+pub struct HttpServer {
+    server: MockServer,
+}
+
+impl HttpServer {
+    pub fn new(filename: &str) -> Self {
+        let server = MockServer::start();
+        let filepath = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+            .join("src")
+            .join("test-files")
+            .join(filename);
+
+        let _mock = server.mock(|when, then| {
+            when.method(GET);
+            then.status(200)
+                .header("content-type", "text/html")
+                .body_from_file(filepath.display().to_string());
+        });
+
+        HttpServer { server }
+    }
+
+    pub fn base_url(&self) -> Url {
+        Url::parse(self.server.base_url().as_str()).unwrap()
+    }
+}
