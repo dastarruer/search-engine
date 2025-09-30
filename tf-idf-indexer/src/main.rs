1973a8b6debd2a66fe41ef3cdcb04e04fd50bbd0
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-09-30 10:03:51 -0400

    Create Term struct

--- tf-idf-indexer/src/main.rs
+++ tf-idf-indexer/src/main.rs
@@ -1,3 +1,14 @@
+struct Term<'a> {
+    term: &'a str,
+    idf: i32,
+}
+
+impl<'a> Term<'a> {
+    fn new(term: &'a str) -> Self {
+        Term { term, idf: 0 }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
