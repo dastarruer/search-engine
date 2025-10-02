c2fb150c0bca3a8845914abb82646c285dc889ec
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-10-02 10:39:18 -0400

    Move everything to lib.rs

---
+++ tf-idf-indexer/src/lib.rs
@@ -0,0 +1,94 @@
+use once_cell::sync::Lazy;
+use scraper::{Html, Selector, html::Select};
+
+static BODY_SELECTOR: Lazy<Selector> = Lazy::new(|| Selector::parse("body").unwrap());
+
+struct Term<'a> {
+    pub term: &'a str,
+
+    /// The inverse document frequency of a term.
+    ///
+    /// This measures how rare a term is across documents. If the term appears in many documents, then the IDF is low. If the term only appears in one or two documents, the IDF is high.
+    idf: f32,
+
+    /// The amount of documents that contain this term. Used for calculating [`Term::idf`].
+    document_frequency: i32,
+}
+
+impl<'a> Term<'a> {
+    fn new(term: &'a str) -> Self {
+        Term {
+            term,
+            idf: 0.0,
+            document_frequency: 0,
+        }
+    }
+
+    /// Find the number of times that a [`Term`] appears in a given piece of text.
+    ///
+    /// This is called the *term frequency* of a term.
+    fn get_tf<'b>(&self, text: Select<'a, 'b>) -> i32 {
+        text.flat_map(|e| e.text()) // flatten text nodes
+            .flat_map(|t| t.split_whitespace()) // flatten words
+            .filter(|word| word == &self.term)
+            .count() as i32
+    }
+
+    fn update_idf(&mut self, num_documents: i32) {
+        self.idf = f32::log10((num_documents / self.document_frequency) as f32);
+    }
+}
+
+/// Return the path of a file in src/test-files given just its filename.
+#[cfg(test)]
+pub fn test_file_path_from_filepath(filename: &str) -> std::path::PathBuf {
+    // CARGO_MANIFEST_DIR gets the source dir of the project
+    std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("src")
+        .join("test-files")
+        .join(filename)
+}
+
+trait ExtractText {
+    fn extract_text<'a, 'b>(&'a self) -> Select<'a, 'static>
+    where
+        'a: 'b;
+}
+
+impl ExtractText for Html {
+    fn extract_text<'a, 'b>(&'a self) -> Select<'a, 'static>
+    where
+        'a: 'b,
+    {
+        self.select(&BODY_SELECTOR)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::fs;
+
+    use scraper::Html;
+
+    use crate::{ExtractText, Term, test_file_path_from_filepath};
+
+    #[test]
+    fn test_get_tf_in_html() {
+        let html = fs::read_to_string(test_file_path_from_filepath("tf.html")).unwrap();
+        let html = Html::parse_document(html.as_str());
+
+        let term = Term::new("hello");
+
+        assert_eq!(term.get_tf(html.extract_text()), 4);
+    }
+
+    #[test]
+    fn test_update_idf() {
+        let mut term = Term::new("hello");
+        term.document_frequency = 2;
+
+        term.update_idf(2);
+
+        assert_eq!(term.idf, 0.0);
+    }
+}
