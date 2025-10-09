b7a9f8d7fef6003e9fcfacbbd42d82a4545de572
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-10-08 20:10:01 -0400

    Extract magic numbers from pool config into vars

--- crawler/src/crawler.rs
+++ crawler/src/crawler.rs
@@ -337,12 +337,20 @@ impl Crawler {
         let url = construct_postgres_url();
         let url = url.as_str();
 
+        let max_connections = 10;
+        let min_connections = 2;
+
+        let connection_timeout = std::time::Duration::from_secs(5);
+
+        let max_lifetime = Some(std::time::Duration::from_secs(1800));
+        let idle_timeout = Some(std::time::Duration::from_secs(600));
+
         let pool = PgPoolOptions::new()
-            .max_connections(10) // maximum connections in the pool
-            .min_connections(2) // keep a few always alive
-            .acquire_timeout(std::time::Duration::from_secs(5)) // connection timeout
-            .max_lifetime(Some(std::time::Duration::from_secs(1800))) // recycle old connections
-            .idle_timeout(Some(std::time::Duration::from_secs(600))) // close idle connections
+            .max_connections(max_connections)
+            .min_connections(min_connections)
+            .acquire_timeout(connection_timeout) // connection timeout
+            .max_lifetime(max_lifetime) // recycle old connections
+            .idle_timeout(idle_timeout) // close idle connections
             .connect(url) // async connect
             .await
             .expect("DATABASE_URL should correctly point to the PostGreSQL database.");
