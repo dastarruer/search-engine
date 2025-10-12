306662eb2eac04450210cae06e3354381257a4bd
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-10-11 23:38:24 -0400

    Add terms table to db

---
+++ flyway/sql/V7__Create_terms_table.sql
@@ -0,0 +1,11 @@
+-- Necessary for the hstore attribute, which is used for key value pairs
+CREATE EXTENSION hstore;
+
+CREATE TABLE term (
+    id serial primary key NOT NULL,
+    term text NOT NULL UNIQUE,
+    idf double precision NOT NULL,
+    page_frequency integer NOT NULL,
+    tf_scores hstore,
+    tf_idf_scores hstore,
+);
