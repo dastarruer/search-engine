16bcc696a3628e3d1470784a61ef20ef1027de3c
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-10-12 22:28:55 -0400

    Remove test pages

--- tf-idf-indexer/src/main.rs
+++ tf-idf-indexer/src/main.rs
@@ -1,28 +1,11 @@
 use std::collections::{HashMap, HashSet};
 
-use scraper::Html;
 use sqlx::postgres::PgPoolOptions;
-use tf_idf_indexer::{Indexer, Page, utils};
+use tf_idf_indexer::{Indexer, utils};
 
 #[tokio::main]
 async fn main() {
     let mut starting_pages = HashSet::new();
-
-    for i in 0..4 {
-        let id = i;
-
-        let page = Page::new(
-            Html::parse_document(
-                r#"
-            <body>
-                <p>hippopotamus hippopotamus hippopotamus</p>
-            </body>"#,
-            ),
-            id,
-        );
-
-        starting_pages.insert(page);
-    }
 
     let mut indexer = Indexer::new(HashMap::new(), starting_pages);
 
