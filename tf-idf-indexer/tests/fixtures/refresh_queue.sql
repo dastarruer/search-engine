e1fdcb6eb83f4352744cecba3797af4eca322f70
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-10-16 08:33:08 -0400

    Remove migrations volume and expand build context

---
+++ tf-idf-indexer/tests/fixtures/refresh_queue.sql
@@ -0,0 +1,5 @@
+INSERT INTO pages (url, html, is_indexed)
+VALUES
+    ('www.does-not-matter.com', '<body><p>hippopotamus hippopotamus hippopotamus</p></body>', TRUE),
+    ('www.does-not-matter.com', '<body><p>hippopotamus hippopotamus hippopotamus</p></body>', TRUE),
+    ('www.does-not-matter.com', '<body><p>hippopotamus hippopotamus hippopotamus</p></body>', TRUE);
