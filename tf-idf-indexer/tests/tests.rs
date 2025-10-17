52b721a854c84722abad406239570bbd23f6db18
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-10-16 21:51:54 -0400

    Test refresh_queue against an array of pages

--- tf-idf-indexer/tests/tests.rs
+++ tf-idf-indexer/tests/tests.rs
@@ -10,15 +10,24 @@ async fn test_refresh_queue() -> sqlx::Result<()> {
     let (_container, pool) = common::setup("refresh_queue").await;
 
     let mut indexer = Indexer::new(HashMap::new(), HashSet::new());
-    let expected_page = Page::new(
-        Html::parse_document("<body><p>hippopotamus hippopotamus hippopotamus</p></body>"),
-        1,
-    );
+
+    let mut expected_pages = vec![];
+    for i in 0..3 {
+        let expected_page = Page::new(
+            Html::parse_document("<body><p>hippopotamus hippopotamus hippopotamus</p></body>"),
+            i + 1,
+        );
+
+        expected_pages.push(expected_page);
+    }
 
     indexer.refresh_queue(&pool).await;
 
-    assert_eq!(indexer.num_pages(), 3);
-    assert!(indexer.contains(&expected_page));
+    assert_eq!(indexer.num_pages(), expected_pages.len() as i32);
+
+    for page in expected_pages {
+        assert!(indexer.contains(&page));
+    }
 
     Ok(())
 }
