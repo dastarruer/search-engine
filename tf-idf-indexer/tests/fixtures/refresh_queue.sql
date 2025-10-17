ac7dfc8927737c3fc1010e537e4a454fed76eb44
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-10-16 21:47:33 -0400

    Remove migrations symlinks

--- tf-idf-indexer/tests/fixtures/refresh_queue.sql
+++ tf-idf-indexer/tests/fixtures/refresh_queue.sql
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 INSERT INTO pages (url, html, is_crawled, is_indexed)
 VALUES
     ('https://url-0.com', '<body><p>hippopotamus hippopotamus hippopotamus</p></body>', TRUE, FALSE),
     ('https://url-1.com', '<body><p>hippopotamus hippopotamus hippopotamus</p></body>', TRUE, FALSE),
     ('https://url-2.com', '<body><p>hippopotamus hippopotamus hippopotamus</p></body>', TRUE, FALSE);
-=======
-INSERT INTO pages (url, html, is_indexed)
-VALUES
-    ('www.does-not-matter.com', '<body><p>hippopotamus hippopotamus hippopotamus</p></body>', TRUE),
-    ('www.does-not-matter.com', '<body><p>hippopotamus hippopotamus hippopotamus</p></body>', TRUE),
-    ('www.does-not-matter.com', '<body><p>hippopotamus hippopotamus hippopotamus</p></body>', TRUE);
->>>>>>> master
