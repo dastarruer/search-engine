b6f0c7c23be64bf82d6ca29803cea093e22ec128
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-10-22 22:42:43 -0400

    Add test to add conflicting term to db

--- services/tf-idf-indexer/tests/indexer/add_term.rs
+++ services/tf-idf-indexer/tests/indexer/add_term.rs
@@ -9,15 +9,9 @@ async fn test_add_term_to_db() {
     let unique_term_str = String::from("america");
     let unique_term = Term::from(unique_term_str.clone());
 
-    let conflicting_term_str = String::from("hippopotamus");
-    let conflicting_term = Term::from(conflicting_term_str.clone());
-
     unique_term.add_to_db(&pool).await;
-    conflicting_term.add_to_db(&pool).await;
 
     let unique_term_err_msg = format!("Term {} should exist in the database.", unique_term_str);
-    let conflicting_term_err_msg =
-        format!("Term {} should exist in the database.", unique_term_str);
 
     let query = r#"SELECT * FROM terms WHERE term = $1"#;
     assert_eq!(
@@ -30,6 +24,23 @@ async fn test_add_term_to_db() {
         ),
         unique_term
     );
+}
+
+#[tokio::test]
+async fn test_add_existing_term_to_db() {
+    let (_container, pool) = common::setup("dummy_data").await;
+
+    let conflicting_term_str = String::from("hippopotamus");
+    let conflicting_term = Term::from(conflicting_term_str.clone());
+
+    conflicting_term.add_to_db(&pool).await;
+
+    let conflicting_term_err_msg = format!(
+        "Term {} should exist in the database.",
+        conflicting_term_str
+    );
+
+    let query = r#"SELECT * FROM terms WHERE term = $1"#;
 
     // Adding the conflicting term should update the term in the database
     assert_eq!(
