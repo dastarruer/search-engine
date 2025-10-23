28a6f7bba84b3932011ac463369b08b00075127c
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-10-23 12:13:22 -0400

    Fix migrations version mismatch

--- migrations/06_create_terms_table.sql
+++ migrations/06_create_terms_table.sql
@@ -1,7 +1,7 @@
 -- Necessary for the hstore attribute, which is used for key value pairs
 CREATE EXTENSION hstore;
 
-CREATE TABLE IF NOT EXISTS terms (
+CREATE TABLE IF NOT EXISTS term (
     id serial primary key NOT NULL,
     term text NOT NULL UNIQUE,
     idf double precision NOT NULL,
---
+++ migrations/08_rename_term_table_to_terms.sql
@@ -0,0 +1,1 @@
+ALTER TABLE term RENAME TO terms;
