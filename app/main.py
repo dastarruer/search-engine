8df24ff48ddc3e65fcf38883ae72395b4fe98f80
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-10-28 08:56:43 -0400

    Add connection to postgres

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 from flask import Flask
+import psycopg2
+
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +10,19 @@ def hello_world():
     return 'Hello World'
 
 if __name__ == '__main__':
+    database = retrieve_env_var("DB_NAME")
+    user = retrieve_env_var("DB_USER")
+    password = retrieve_env_var("DB_PASSWORD")
+    host = retrieve_env_var("DB_ENDPOINT")
+
+    conn = psycopg2.connect(database=database, user=user,
+                            password=password, host=host, port="5432")
+    cur = conn.cursor()
+
     app.run()
+
+def retrieve_env_var(var: str) -> str:
+    try:
+        return os.environ[key]
+    except KeyError:
+        raise RuntimeError(f"Missing required environment variable: {key}")
