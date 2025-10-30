3ee9fe73fea3a1378d1e37bbd25bd9551726f76b
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-10-30 13:56:01 -0400

    WIP: Add function to get database connection

--- app/main.py
+++ app/main.py
@@ -11,10 +11,22 @@ def hello_world():
 
 @app.route('/search')
 def search_results():
+    conn = db_conn()
+
     query = request.args.get('q')
     return str(query)
 
 if __name__ == '__main__':
+    app.run()
+
+def retrieve_env_var(var: str) -> str:
+    try:
+        return os.environ[var]
+    except KeyError:
+        raise RuntimeError(f"Missing required environment variable: {var}")
+
+# TODO: Add return type
+def db_conn():
     database = retrieve_env_var("DB_NAME")
     user = retrieve_env_var("DB_USER")
     password = retrieve_env_var("DB_PASSWORD")
@@ -22,14 +34,5 @@ if __name__ == '__main__':
 
     conn = psycopg2.connect(database=database, user=user,
                             password=password, host=host, port="5432")
-    cur = conn.cursor()
+    return conn.cursor()
 
-    app.run()
-
-def retrieve_env_var(var: str) -> str:
-    try:
-        return os.environ[key]
-    except KeyError:
-        raise RuntimeError(f"Missing required environment variable: {key}")
-
-def db_conn() 
