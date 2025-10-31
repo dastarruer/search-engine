5a2166476948d0cd376a3db39b94cc97d06d34a4
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-10-30 20:26:57 -0400

    Format main.py

--- app/main.py
+++ app/main.py
@@ -1,23 +1,28 @@
-from flask import Flask, request
-import psycopg2
-
 import os
+
+import psycopg2
+from psycopg2.extensions import cursor
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-    return 'Hello World'
+    return "Hello World"
 
-@app.route('/search')
+
+@app.route("/search")
 def search_results():
-    conn = db_conn()
+    _conn = db_conn()
 
-    query = request.args.get('q')
+    query = request.args.get("q")
     return str(query)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
+
 
 def retrieve_env_var(var: str) -> str:
     try:
@@ -25,14 +30,17 @@ def retrieve_env_var(var: str) -> str:
     except KeyError:
         raise RuntimeError(f"Missing required environment variable: {var}")
 
-# TODO: Add return type
-def db_conn():
+
+def db_conn() -> cursor:
     database = retrieve_env_var("DB_NAME")
     user = retrieve_env_var("DB_USER")
     password = retrieve_env_var("DB_PASSWORD")
     host = retrieve_env_var("DB_ENDPOINT")
 
-    conn = psycopg2.connect(database=database, user=user,
-                            password=password, host=host, port="5432")
-    return conn.cursor()
+    conn = psycopg2.connect(
+        database=database, user=user, password=password, host=host, port="5432"
+    )
 
+    cursor = conn.cursor()
+
+    return cursor
